c32b19cd38eed1096bc993146e5ec2be12d3958d
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-04-25 18:05:14 +0300

    migrated from h2 to mysql

--- src/main/java/com/litwnb/spring6restmvc/domain/Beer.java
+++ src/main/java/com/litwnb/spring6restmvc/domain/Beer.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -20,7 +22,8 @@ import java.util.UUID;
 public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     @Version
     private Integer version;
