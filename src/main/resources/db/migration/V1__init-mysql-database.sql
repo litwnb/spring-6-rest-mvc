f3223d8d46a47ad1560a4427a641feac2445319f
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-04-28 17:18:38 +0300

    added flyway migration tool

---
+++ src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -0,0 +1,14 @@
+drop table if exists beer;
+
+create table beer (
+                      id varchar(36) not null,
+                      beer_name varchar(50) not null,
+                      beer_style smallint not null,
+                      created_date datetime(6),
+                      price decimal(38,2) not null,
+                      quantity_on_hand integer,
+                      upc varchar(255) not null,
+                      update_date datetime(6),
+                      version integer,
+                      primary key (id)
+) engine=InnoDB;
